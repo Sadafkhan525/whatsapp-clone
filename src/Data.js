876a7e247b5ebc49e15dfcb42f05c2df5b89b1e3
unknown <sadafkhan.sk688@gmail.com>
2023-02-07 14:57:40 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,75 @@
+export const contactList = [
+    {
+      id: 1,
+      name: "Shalini",
+      profilePic: "/profile/pic5.jpg",
+      lastText: "Hello Kya kr rhe ho",
+      lastTextTime: "09:58 PM",
+    },
+    {
+      id: 2,
+      name: "Ammy ji",
+      profilePic: "/profile/pic2.jpg",
+      lastText: `Kaise ho beta`,
+      lastTextTime: "10:45 PM",
+    },
+    {
+      id: 3,
+      name: "Shifa Khan",
+      profilePic: "/profile/pic1.jpg",
+      lastText: "Kha ho",
+      lastTextTime: "12:45 PM",
+    },
+    {
+      id: 4,
+      name: "Papa Ji",
+      profilePic: "/profile/pic3.jpg",
+      lastText: "https://facebook.com",
+      lastTextTime: "08:52 PM",
+    },
+  ];
+  export const messagesList = [
+    {
+      id: 1,
+      messageType: "TEXT",
+      text: "hello",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 2,
+      messageType: "TEXT",
+      text: "What's up?",
+      senderID: 1,
+      addedOn: "12:01 PM",
+    },
+    {
+      id: 3,
+      messageType: "TEXT",
+      text: "All Good, What about you?",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 4,
+      messageType: "TEXT",
+      text: "I'm good as well",
+      senderID: 1,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 5,
+      messageType: "TEXT",
+      text: "Great 😁",
+      senderID: 0,
+      addedOn: "12:00 PM",
+    },
+    {
+      id: 5,
+      messageType: "TEXT",
+      text: "Subscribed to Simplilarn",
+      senderID: 1,
+      addedOn: "12:00 PM",
+    },
+  ];
+  
---
+++ src/components/Conversation.js
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import { messagesList } from "../Data";
+import { SearchContainer, SearchInput } from "./ContactList";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  flex: 2;
+  background-color: #f6f7f8;
+`;
+
+const ProfileHeader = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #ededed;
+  padding: 15px;
+  align-items: center;
+  gap: 10px;
+`;
+const ProfileImage = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+`;
+
+const ChatBox = styled.div`
+  display: flex;
+  background: #f0f0f0;
+  padding: 10px;
+  align-items: center;
+  bottom: 0;
+`;
+
+const EmojiImage = styled.img`
+  width: 30px;
+  height: 28px;
+  opacity: 0.4;
+  cursor : Pointer;
+`;
+
+const MessageDiv = styled.div`
+display : flex;
+justify-content :${(props) => (props.isYours ? 'flex-end' : 'flex-start')} ;
+margin: 5px 10px;
+
+`;
+const MessageContainer = styled.div`
+display : flex;
+flex-direction : column;
+height : 100%;
+`;
+
+const Message = styled.div`
+justify-content :${(props) => (props.isYours ? "#daf8cb" : "white")} ;
+max-width : 50%;
+background:#daf8cb;
+color: #303030;
+font-size : 14px;
+padding : 8px 10px;
+font-size:19px;
+`;
+const Conversation = () => {
+    return (
+        <Container>
+            <ProfileHeader>
+                <ProfileImage src="/profile/pic6.jpg" />
+                Shalini
+            </ProfileHeader>
+            <MessageContainer>
+                {messagesList.map((messageData) => (
+                    <MessageDiv isYours={messageData.senderID === 0}>
+                        <Message isYours={messageData.senderID === 0} > {[messageData.text]} </Message>
+                    </MessageDiv>
+                ))}
+
+            </MessageContainer>
+            <ChatBox>
+                <SearchContainer>
+                    <EmojiImage src={"/profile/data.svg"} />
+                    <SearchInput placeholder="Type a message..." />
+                </SearchContainer>
+            </ChatBox>
+        </Container>
+    );
+};
+
+export default Conversation;
